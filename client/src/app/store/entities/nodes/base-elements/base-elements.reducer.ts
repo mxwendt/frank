ceec7562795d7404ac30fa032b6433535d511bb4
Maximilian <mxwendt@gmail.com>
2019-12-15 15:58:30 +0100

    Fix issue with base elements default values

--- client/src/app/store/entities/nodes/base-elements/base-elements.reducer.ts
+++ client/src/app/store/entities/nodes/base-elements/base-elements.reducer.ts
@@ -16,7 +16,7 @@ export const initialState: State = adapter.getInitialState({
   // additional entity state properties
 
   // default values
-  ids: ['0', '1'],
+  ids: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28'],
   entities: {
     '0': {
       id: '0',
@@ -45,7 +45,386 @@ export const initialState: State = adapter.getInitialState({
           lineHeight: '0'
         }
       }
+    },
+    '2': {
+      id: '2',
+      selector: 'footer',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '3': {
+      id: '3',
+      selector: 'header',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '4': {
+      id: '4',
+      selector: 'h1',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '5': {
+      id: '5',
+      selector: 'h2',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '6': {
+      id: '6',
+      selector: 'h3',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '7': {
+      id: '7',
+      selector: 'h4',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '8': {
+      id: '8',
+      selector: 'h5',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '9': {
+      id: '9',
+      selector: 'h6',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '10': {
+      id: '10',
+      selector: 'hgroup',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '11': {
+      id: '11',
+      selector: 'main',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '12': {
+      id: '12',
+      selector: 'nav',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '13': {
+      id: '13',
+      selector: 'section',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '14': {
+      id: '14',
+      selector: 'blockquote',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '15': {
+      id: '15',
+      selector: 'dir',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '16': {
+      id: '16',
+      selector: 'div',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '17': {
+      id: '17',
+      selector: 'dl',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '18': {
+      id: '18',
+      selector: 'dt',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '19': {
+      id: '19',
+      selector: 'dd',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '20': {
+      id: '20',
+      selector: 'figure',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '21': {
+      id: '21',
+      selector: 'figcaption',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '22': {
+      id: '22',
+      selector: 'hr',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '23': {
+      id: '23',
+      selector: 'ol',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '24': {
+      id: '24',
+      selector: 'li',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '25': {
+      id: '25',
+      selector: 'p',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '26': {
+      id: '26',
+      selector: 'pre',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '27': {
+      id: '27',
+      selector: 'ul',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
+    },
+    '28': {
+      id: '28',
+      selector: 'li',
+      declarations: {
+        fonts: {
+          fontFamily: '0',
+          fontSize: '0',
+          fontStyle: '0',
+          fontVariant: '0',
+          fontWeight: '0',
+          lineHeight: '0'
+        }
+      }
     }
+
   }
 
 });
